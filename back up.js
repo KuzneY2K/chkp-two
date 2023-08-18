dfd562338b35e507780db258e8d9b578be2eb08a
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-08-18 01:49:06 -0600

    fixed major math bug

--- back up.js
+++ back up.js
@@ -21,60 +21,36 @@ let clickUpgrades = [{
     name: 'Cheese Knife',
     price: 50,
     quantity: 1,
-    multiplier: 0,
-}, {
-    name: 'Big Cheese Knife',
-    price: 200,
-    quantity: 0,
-    multiplier: 0,
+    multiplier: 1,
 }]
 
 let autoUpgrades = [{
     name: 'Friend',
     price: 200,
     quantity: 1,
-    multiplier: 0,
-}, {
-    name: 'Hungry Friends',
-    price: 800,
-    quantity: 0,
-    multiplier: 0,
+    multiplier: 2,
 }]
 
 // Functions
 function mine() {
-    cheese += clickUpgrades[0].quantity + clickUpgrades[1].quantity * clickUpgrades[0].multiplier + clickUpgrades[1].multiplier
+    cheese += clickUpgrades[0].quantity * clickUpgrades[0].multiplier
     updateDOM()
 }
 
 function updateDOM() {
     cheeseOutput.innerHTML = cheese
     clickUpgradeCostOne.innerHTML = clickUpgrades[0].price
-    clickUpgradeCostTwo.innerHTML = clickUpgrades[1].price
+    clickUpgradeCostTwo.innerHTML = clickUpgrades[0].price * 2
     autoUpgradeCostOne.innerHTML = autoUpgrades[0].price
-    autoUpgradeCostTwo.innerHTML = autoUpgrades[1].price
+    autoUpgradeCostTwo.innerHTML = autoUpgrades[0].price * 4
 }
 
 function buyCheeseKnife() {
     if (cheese >= clickUpgrades[0].price) {
         cheese -= clickUpgrades[0].price
         clickUpgrades[0].quantity += 1
-        clickUpgrades[0].multiplier += 1
-        clickUpgrades[0].price *= clickUpgrades[0].quantity
+        clickUpgrades[0].price *= 2
     } else if (cheese < clickUpgrades[0].price) {
-        buyKnifeButton.classList.toggle('disabled')
-    }
-    // DISABLE BUTTONS IS CHEESE IS LESS
-    updateDOM()
-}
-
-function buyMoreCheeseKnife() {
-    if (cheese >= clickUpgrades[1].price) {
-        cheese -= clickUpgrades[1].price
-        clickUpgrades[1].quantity += 1
-        clickUpgrades[1].multiplier += 1
-        clickUpgrades[1].price *= clickUpgrades[1].quantity
-    } else if (cheese < clickUpgrades[1].price) {
         buyKnifeButton.classList.toggle('disabled')
     }
     // DISABLE BUTTONS IS CHEESE IS LESS
@@ -85,7 +61,6 @@ function buyFriend() {
     if (cheese >= autoUpgrades[0].price) {
         cheese -= autoUpgrades[0].price
         autoUpgrades[0].quantity += 1
-        autoUpgrades[0].multiplier += 1
         autoUpgrades[0].price *= autoUpgrades[0].quantity
     } else if (cheese < clickUpgrades[0].price) {
         buyKnifeButton2.classList.toggle('disabled')
@@ -93,50 +68,58 @@ function buyFriend() {
     updateDOM()
 }
 
-function buyHungryFriends() {
-    if (cheese >= autoUpgrades[1].price) {
-        cheese -= autoUpgrades[1].price
-        autoUpgrades[1].quantity += 1
-        autoUpgrades[1].multiplier += 1
-        autoUpgrades[1].price *= autoUpgrades[1].quantity
-    } else if (cheese < clickUpgrades[1].price) {
-        buyKnifeButton2.classList.toggle('disabled')
+function buyMoreKnives() {
+    if (cheese >= clickUpgrades[0].price * 5) {
+        cheese -= clickUpgrades[0].price * 5
+        clickUpgrades[0].quantity += 5
+        clickUpgrades[0].price *= 4
+    } else if (cheese < autoUpgrades[0].price) {
+        buyFriendsButton.classList.toggle('disabled')
+    }
+    updateDOM()
+}
+
+function buyMoreFriends() {
+    if (cheese >= autoUpgrades[0].price * 5) {
+        cheese -= clickUpgrades[0].price * 5
+        autoUpgrades[0].quantity += 5
+        autoUpgrades[0].price *= autoUpgrades[0].quantity
+    } else if (cheese < autoUpgrades[0].price) {
+        buyFriendsButton2.classList.toggle('disabled')
     }
     updateDOM()
 }
 
 // Intervals
 
-// Auto Mine
-
 setInterval(function () {
-    cheese += autoUpgrades[0].quantity + autoUpgrades[1].quantity * autoUpgrades[0].multiplier + autoUpgrades[1].multiplier
+    cheese += autoUpgrades[0].quantity * autoUpgrades[0].multiplier
     updateDOM()
 }, 1000)
 
 setInterval(function () {
-    cheesePerClickOut.innerHTML = clickUpgrades[0].quantity + clickUpgrades[1].quantity * clickUpgrades[0].multiplier + clickUpgrades[1].multiplier
-    cheesePerSecOut.innerHTML = autoUpgrades[0].quantity + autoUpgrades[1].quantity * autoUpgrades[0].multiplier + autoUpgrades[1].multiplier
+    cheesePerClickOut.innerHTML = clickUpgrades[0].quantity * clickUpgrades[0].multiplier
+    cheesePerSecOut.innerHTML = autoUpgrades[0].quantity * autoUpgrades[0].multiplier
 }, 100)
 
 setInterval(function () {
-    qtyKnivesOut.innerHTML = clickUpgrades[0].quantity + clickUpgrades[1].quantity
-    qtyFriendsOut.innerHTML = autoUpgrades[0].quantity + autoUpgrades[1].quantity
-    qtyKnivesPC.innerHTML = clickUpgrades[0].quantity + clickUpgrades[1].quantity * clickUpgrades[0].multiplier + clickUpgrades[0].multiplier
-    qtyFriendsPS.innerHTML = autoUpgrades[0].quantity + autoUpgrades[1].quantity * autoUpgrades[0].multiplier + autoUpgrades[1].multiplier
+    qtyKnivesOut.innerHTML = clickUpgrades[0].quantity
+    qtyFriendsOut.innerHTML = autoUpgrades[0].quantity
+    qtyKnivesPC.innerHTML = clickUpgrades[0].quantity * clickUpgrades[0].multiplier
+    qtyFriendsPS.innerHTML = autoUpgrades[0].quantity * autoUpgrades[0].multiplier
 }, 100)
 
 setInterval(function () {
     if (cheese < clickUpgrades[0].price) {
         buyKnifeButton.classList.add('disabled')
     }
-    if (cheese < clickUpgrades[1].price) {
+    if (cheese < clickUpgrades[0].price * 5) {
         buyKnifeButton2.classList.add('disabled')
     }
     if (cheese >= clickUpgrades[0].price) {
         buyKnifeButton.classList.remove('disabled')
     }
-    if (cheese >= clickUpgrades[1].price) {
+    if (cheese >= clickUpgrades[0].price * 5) {
         buyKnifeButton2.classList.remove('disabled')
     }
 }, 100)
@@ -145,13 +128,13 @@ setInterval(function () {
     if (cheese < autoUpgrades[0].price) {
         buyFriendsButton.classList.add('disabled')
     }
-    if (cheese < autoUpgrades[1].price) {
+    if (cheese < autoUpgrades[0].price * 5) {
         buyFriendsButton2.classList.add('disabled')
     }
     if (cheese >= autoUpgrades[0].price) {
         buyFriendsButton.classList.remove('disabled')
     }
-    if (cheese >= autoUpgrades[1].price) {
+    if (cheese >= autoUpgrades[0].price * 5) {
         buyFriendsButton2.classList.remove('disabled')
     }
 }, 100)
